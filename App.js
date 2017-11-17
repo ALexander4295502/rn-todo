7ad4f373bc8fcd7f616270795028c80fdbb84a01
ALexander4295502 <yuanzhengstl@gmail.com>
2017-11-16 23:32:45 -0600

    create textInput

--- App.js
+++ App.js
@@ -8,17 +8,46 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Platform
+  Platform,
 } from 'react-native';
 
 import Footer from './Components/footer';
 import Header from './Components/header';
 
 export default class App extends Component<{}> {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      value: "",
+      items: []
+    }
+  }
+
+  handleAddItem() {
+    if(!this.state.value) return;
+    const newItem = [
+      ...this.state.items,
+      {
+        key: Date.now(),
+        text: this.state.value,
+        complete: false
+      }
+    ];
+    this.setState({
+      items: newItem,
+      value: ""
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header/>
+        <Header
+          value={this.state.value}
+          onAddItem={this.handleAddItem.bind(this)}
+          onChange={(value) => this.setState({value})}
+        />
         <View style={styles.content}>
 
         </View>
--- Components/header.js
+++ Components/header.js
@@ -1,17 +1,42 @@
 import React, {Component} from 'react';
 import {
   StyleSheet,
-  View
+  View,
+  TextInput
 } from 'react-native';
 
 export default class Header extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() {
     return (
-      <View></View>
+      <View style={styles.header}>
+        <TextInput
+          value={this.props.value}
+          onChangeText={this.props.onChange}
+          onSubmitEditing={this.props.onAddItem}
+          placeholder="What needs to be done?"
+          blurOnSubmit={false}
+          returnKeyTupe="done"
+          style={styles.input}
+        />
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {}
+  header: {
+    paddingHorizontal: 16,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input: {
+    flex: 1,
+    height: 50,
+  }
 });
